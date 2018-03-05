b7612186e13f591b551143211b8b7578aa35e4ba
jurajmajerik <juro.majerik@gmail.com>
2018-03-05 23:28:49 +0700

    compose basic layout

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+var contentNode = document.getElementById('contents');
+
+class IssueFilter extends React.Component {
+  render() {
+    return(
+      <div>This is a placeholder for the Issue Filter.</div>
+    );
+  }
+}
+
+class IssueRow extends React.Component {
+  render() {
+    const borderedStyle = {border: '1px solid silver', padding: 4};
+    return(
+      <tr>
+        <td style={borderedStyle}>{this.props.issue_id}</td>
+        <td style={borderedStyle}>{this.props.issue_title}</td>
+      </tr>
+    );
+  }
+}
+
+class IssueTable extends React.Component {
+  render() {
+    const borderedStyle = {border: '1px solid silver', padding: 6};
+    return(
+      <table style={{borderCollapse: 'collapse'}}>
+        <thead>
+          <tr>
+            <th style={borderedStyle}>Id</th>
+            <th style={borderedStyle}>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <IssueRow issue_id={1} issue_title='First issue' />
+          <IssueRow issue_id={2} issue_title='Second issue' />
+        </tbody>
+      </table>
+    );
+  }
+}
+
+class IssueAdd extends React.Component {
+  render() {
+    return(
+      <div>This is the placeholder for the Issue Add Entry Form.</div>
+    );
+  }
+}
+
+class IssueList extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable />
+        <hr />
+        <IssueAdd />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<IssueList />, contentNode);
